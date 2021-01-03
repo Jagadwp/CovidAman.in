405ae6576e2daff0a4fd1e0418d6a9a7dfe53afc
Jagad Wijaya Purnomo <Jagadpurnomo26@gmail.com>
2021-01-03 12:49:20 +0700

    Add 2 pages

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,47 +2,46 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-
 module.exports = {
-    entry: "./src/index.js",
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: "./src/template.html",
-            filename: "index.html"
-        })
-    ],
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js"
-    },
-    mode: "production",
-    module: {
-        rules: [
-            /* style and css loader */
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            },
-            // html-loader
-            {
-                test: /\.html$/,
-                use: ["html-loader"]
-            },
-            //file loader
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            name: '[name].[hash].[ext]',
-                            outputPath: 'imgs',
-                            esModule: false
-                        }
-                    }
-                ]
-            }
-        ]
-    }
-}; 
+	entry: "./src/index.js",
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: "./src/template.html",
+			filename: "index.html",
+		})
+	],
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	mode: "production",
+	module: {
+		rules: [
+			/* style and css loader */
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+			// html-loader
+			{
+				test: /\.html$/,
+				use: ["html-loader"],
+			},
+			//file loader
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					{
+						loader: "file-loader",
+						options: {
+							name: "[name].[ext]",
+							outputPath: "imgs",
+							esModule: false,
+						},
+					},
+				],
+			},
+		],
+	},
+};
